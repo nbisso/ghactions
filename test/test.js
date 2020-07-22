3bcca7e87f0d83b07bbcd423e017c18c8ae12f36
nbisso <nicolas.bisso@mercadolibre.com>
2020-07-22 16:18:03 -0300

    test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../server");
 
 describe("GET /home", () => {
-    it("respond with Hello World", (done) => {
+    it("respond with its works!", (done) => {
         request(app).get("/home").expect("its works!", done);
     })
 });
