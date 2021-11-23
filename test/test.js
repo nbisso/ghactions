fd453a5d41f869457e398ccab261aada163f8093
Juan Pablo Millicay <juan22milli@gmail.com>
2021-11-23 12:54:24 -0300

    test modificado

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../server");
+const app = require("./server");
 
 describe("GET /home", () => {
     it("respond with its works!", (done) => {
